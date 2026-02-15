14cbcb07518de1b7afd3e431da768e388379031a
Aida <aida.avetisyan93@gmail.com>
2026-02-15 15:08:06 +0400

    add adsense tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
 import type { Metadata } from "next"
 import React from "react"
+import Script from "next/script";
 
 export const metadata: Metadata = {
     title: "Block Puzzle Game",
     description: "Play Block Puzzle Online",
+    other: {
+        "google-adsense-account": "ca-pub-2296093498264168",
+    },
 }
 
 export default function RootLayout({
@@ -14,6 +18,12 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body>
+        <Script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2296093498264168"
+            crossOrigin="anonymous"
+            strategy="afterInteractive"
+        />
         {children}
         </body>
         </html>
